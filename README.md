42630b2b1e3fd9427348575c4981231f162325f9
Joakim Ewenson <joakim@ewenson.se>
2021-08-19 12:20:25 +0200

    WIP

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Embedded traffic information for Swedish trains and train stations
+_Information will follow in Swedish for the rest of this README_
+
+Detta är en lekstuga med React för att bygga en trafikinformationstjänst för järnväg anpassad för visning i olika former av statiska skärmar. Navigation och anpassning av sker via URL, för att enkelt kunna visas i kioskklienter.
+
+Tjänsten använder sig av stationsförkortningar utifrån Trafikverkets databas, Stockholm C har exempelvis 'Cst', Göteborg har 'G' och Malmö har 'M'. Komplett lista går att få ut från Trafikverkets öppna API vid behov.
+
+## Funktionalitet
+* Visa tågs tidtabell och aktuella position.
+* Visa ankomster och avgångar vid särskild station.
+* Visa antingen ankomster eller avgångar vid särskild station, för separat visning på flera skärmar.
+* Visa aktuella trafikmeddelanden som berör en särskild station.
+
+## URL endpoints
+* /train/12345 - visar tidtabell och position
+* /station/Abc - visar ankomster och avgångar vid stationssignatur Abc
+* /station/Abc/arrivals - visar ankomster vid stationssignatur Abc
+* /station/Abc/departures - visar avgångar vid stationssignatur Abc
+* /msg/Abc - visar trafikmeddelanden vid stationssignatur Abc
+
+## Krav för att köra applikationen
+För att få tillgång till Trafikverkets öppna API krävs en API-nyckel som du kan få via https://api.trafikinfo.trafikverket.se. Denna placeras sedan i variabeln _REACT_APP_TRV_APIKEY_ i en .env-fil eller i miljövariablerna på systemet som skall köra applikationen.
--- src/App.js
+++ src/App.js
@@ -1,36 +1,97 @@
 import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Switch, Route, useParams } from "react-router-dom";
 import "./App.css";
 import { fetchTrainMessages } from "./services/fetchTrainMessages";
 
-function App() {
-  const [messages, setMessages] = useState({})
-  const locationId = 'M'
-
-  async function getMessages() {
-    const result = await fetchTrainMessages(locationId)
-    setMessages(result)
-  }
-
-  useEffect(() => {
-    getMessages()
-  },[locationId])
-
-  console.log(messages)
-  
+export default function App() {
   return (
     <div className="App">
-      {messages && (
-        <>
-        {messages.TrainMessage?.map((msg) => (
-          <div className="trainMessageCard">
-            <h4 key={Math.random()}>{msg.Header}</h4>
-            <p>{msg.ExternalDescription}</p>
-          </div>
-        ))}
-        </>
-      )}
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <></>
+          </Route>
+          <Route exact path="/train/:trainIdent/:searchDate?">
+            <TrainPage />
+          </Route>
+          <Route exact path="/station/:locationId">
+            <StationPage />
+          </Route>
+          <Route exact path="/station/:locationId/:type?">
+            <StationPage />
+          </Route>
+          <Route exact path="/msg/:locationId">
+            <TrainMessagePage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
 
-export default App;
+function StationPage() {
+  const { locationId, type } = useParams();
+
+  return (
+    <div>
+      <p>
+        Location ID: {locationId}
+        {type && (
+          <>
+            <br />
+            Type: {type}
+          </>
+        )}
+      </p>
+    </div>
+  );
+}
+
+function TrainPage() {
+  const { trainIdent, searchDate } = useParams();
+  return (
+    <div>
+      <p>
+        Train ident: {trainIdent}
+        {searchDate && (
+          <>
+            <br />
+            Search date: {searchDate}
+          </>
+        )}
+      </p>
+    </div>
+  );
+}
+
+function TrainMessagePage() {
+  const { locationId } = useParams();
+  const [messages, setMessages] = useState({});
+
+  
+  useEffect(() => {
+    async function getMessages() {
+      const result = await fetchTrainMessages(locationId);
+      setMessages(result);
+    }
+    getMessages();
+  }, [locationId]);
+
+  console.log(messages);
+
+  return (
+    <>
+      <h4>Visar trafikstörningsmeddelanden för {locationId}</h4>
+      {messages && (
+        <>
+          {messages.TrainMessage?.map((msg) => (
+            <div className="trainMessageCard">
+              <h4 key={Math.random()}>{msg.Header}</h4>
+              <p>{msg.ExternalDescription}</p>
+            </div>
+          ))}
+        </>
+      )}
+    </>
+  );
+}
