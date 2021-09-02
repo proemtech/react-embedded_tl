d1e639e7824800ce129723f629798f894ac33860
Joakim Ewenson <34868402+JoakimEwenson@users.noreply.github.com>
2021-09-02 23:06:07 +0200

    Added trainStreamQuery
    For later use in getting a better stream

---
+++ src/services/queries/trainStreamQuery.js
@@ -0,0 +1,15 @@
+export const trainStatusQuery = (trainIdent, searchDate) => `
+    <REQUEST>
+      <LOGIN authenticationkey="${process.env.REACT_APP_TRV_APIKEY}" />
+      <QUERY objecttype="TrainAnnouncement" schemaversion="1.6" sseurl="true">
+            <FILTER>
+                <AND>
+                  <OR>
+                      <EQ name="AdvertisedTrainIdent" value="${trainIdent}" />
+                      <EQ name="TechnicalTrainIdent" value="${trainIdent}" />
+                  </OR>
+                  <EQ name="ScheduledDepartureDateTime" value="${searchDate}" />
+                </AND>
+            </FILTER>
+      </QUERY>
+    </REQUEST>`
